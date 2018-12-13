3dd1f06e6f5d1265527012c3e40dbd8e03c3a0d8
nicomateucci <nico-mateucci@hotmail.com>
2018-12-13 11:55:41 -0300

    Agregue manualmente ServletBuscarServicios poruqe el merge no la unio, solo copio las clases nuevas

--- TerminalTPJava/src/servlet/ServletBuscarServicios.java
+++ TerminalTPJava/src/servlet/ServletBuscarServicios.java
@@ -14,8 +14,9 @@ import business.LogicDestino;
 import business.LogicServicio;
 import entities.Destino;
 import entities.Servicio;
-import entities.Usuario;
 import util.AppDataException;
+import util.NoDestinoException;
+import util.NoServiceException;
 
 /**
  * Servlet implementation class ServletBuscarServicios
@@ -37,59 +38,44 @@ public class ServletBuscarServicios extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		Destino d = null;
-		Destino d2 = null;
+		Destino origen = null;
+		Destino destino = null;
 		ArrayList<Servicio> ss = null;
 		LogicDestino logd = new LogicDestino();
-		Usuario  user = (Usuario) request.getSession().getAttribute("usuarioLogeado");
 
-		String des = request.getParameter("textOrigen");
+		String orig = request.getParameter("textOrigen");
+		String dest = request.getParameter("textDestino");
 		try {
-			if((d = logd.getByNombre(des)) != null) {
-				des = request.getParameter("textDestino");
-				if((d2 = logd.getByNombre(des)) != null) {
-					LogicServicio logSer = new LogicServicio();
-					
-					//Linea para agregar en el metodo que tambien acepta una fecha como parametro;
-					//java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
-					
-					ss = logSer.getAllByDestinos(d, d2);
-					if(!ss.isEmpty()) {
-						request.getSession().setAttribute("desOrigen",d);
-						request.getSession().setAttribute("desLlegada",d2);
-						request.getSession().setAttribute("serviciosEcontrados", ss);
-						response.sendRedirect("pages/listarServiciosEcontrados.jsp");
-						
-					}else {
-						if(user != null) {
-							response.sendRedirect("welcome.jsp");
-						} else {
-							response.sendRedirect("index.jsp");
-						}
-					}
-				}else {
-					if(user != null) {
-						response.sendRedirect("welcome.jsp");
-					} else {
-						response.sendRedirect("index.jsp");
-					}
-				}
-			}else {
-				/*Este Javascript anda bian, pero no es conveniente segun Meca.
-				 * PrintWriter out = response.getWriter();  
-				response.setContentType("text/html");  
-				out.println("<script type=\"text/javascript\">");  
-				out.println("alert('Aun no tenemos servicio para el origen ingresado. En breve ampliaremos nuestros recorridos. Nos vemos luego!.');");  
-				out.println("</script>");
-				RequestDispatcher rd=request.getRequestDispatcher("index.jsp");
-				rd.include(request, response);*/
-				response.sendRedirect("./index.jsp");
-			}
+			origen = logd.getByNombre(orig);
+			destino = logd.getByNombre(dest);
+			System.out.println(origen.getLocalidad());
+			System.out.println(destino.getLocalidad());
+			LogicServicio logSer = new LogicServicio();
+			ss = logSer.getAllByDestinos(origen, destino);
+			
+			request.getSession().setAttribute("mensaje", null);
+			request.getSession().setAttribute("desOrigen",origen);
+			request.getSession().setAttribute("desLlegada",destino);
+			request.getSession().setAttribute("serviciosEcontrados", ss);
+			response.sendRedirect("pages/listarServiciosEcontrados.jsp");
+		
+			
 		} catch (AppDataException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 
 			e.printStackTrace();
+		} catch (NoServiceException e) {
+			
+			request.getSession().setAttribute("mensaje", e.getMessage());
+			//System.out.println("No se encontro servicio");
+			response.sendRedirect("index.jsp#services");
+			
+			//e.printStackTrace();
+		} catch (NoDestinoException e) {
+			request.getSession().setAttribute("mensaje", e.getMessage());
+			//System.out.println("No se encontro origen")/destino;
+			response.sendRedirect("index.jsp#services");
 		}
 
 	}
